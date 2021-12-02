eb8cbbdfee67b0d5c8e859de4fca8887a9617940
Rose Luu <luuhongnhung10@gmail.com>
2021-12-01 19:16:50 -0800

    deoloying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/photophinder",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false,
   }
 );
