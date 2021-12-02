080027aae61f2a39459aea284a5162873510426a
Rose Luu <luuhongnhung10@gmail.com>
2021-12-02 11:38:19 -0800

    chang sth to be able to deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/photophinder",
   {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false,
   }
 );
